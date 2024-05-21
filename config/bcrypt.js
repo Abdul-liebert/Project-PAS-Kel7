2ad95e141e879c81b3b3f8f4e8bbe9a5eefbe214
Hanif Gibran <sourcegibran@gmail.com>
2024-05-21 08:23:58 +0700

    made routes

---
+++ config/bcrypt.js
@@ -0,0 +1,16 @@
+const bcrypt = require('bcryptjs');
+
+const hashPassword = async (Password) => {
+    const salt = await bcrypt.hash(Password, 16);
+    return salt;
+}
+
+const comparePassword = async (Password, hashPassword) => {
+    const comparePassword = await bcrypt.hash(Password, hashPassword);
+    return comparePassword;
+}
+
+module.exports = {
+    hashPassword,
+    comparePassword
+}
